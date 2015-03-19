59b33f116880ce6b70f249adc66ef3c623eee8db
Tunelko <tunelko@tunelko.com>
2015-03-19 13:29:50 +0100

    changes on sh

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ FROM debian:stable
 ENV DEBIAN_FRONTEND noninteractive
 RUN apt-get update && \
   apt-get -y install supervisor git apache2 libapache2-mod-php5 curl \ 
-  mysql-server php5-mysql pwgen php5-mcrypt mysql-client-5.5 mysql-common mysql-common mysql-server-5.5 mysql-server-core-5.5  && \
+  	mysql-server php5-mysql pwgen php5-mcrypt mysql-client-5.5 mysql-common mysql-common mysql-server-5.5 mysql-server-core-5.5  && \
   echo "ServerName localhost" >> /etc/apache2/apache2.conf
 
 
--- allmysql.sh
+++ allmysql.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
+service mysql start 
 
-RET=1
-while [[ RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -uroot -e "status" > /dev/null 2>&1
-    RET=$?
-done
+#/usr/bin/mysqld_safe > /dev/null 2>&1 &
+#RET=1
+#while [[ RET -ne 0 ]]; do
+#    echo "=> Waiting for confirmation of MySQL service startup"
+#    sleep 5
+#    mysql -uroot -e "status" > /dev/null 2>&1
+#    RET=$?
+#done
 
 PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
 _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
