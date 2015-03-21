edf333157c1f079d002b8a71d3d49cc7a480c775
Tunelko <tunelko@tunelko.com>
2015-03-21 15:17:25 +0100

    db

--- src/classes/db.php
+++ src/classes/db.php
@@ -3,4 +3,7 @@
     $lnk = mysql_connect("localhost", "tunelko", "dhWuSEKR7LCgibBt");
     $db = mysql_select_db('ecb', $lnk);
 
+$mysql = new mysqli("localhost", "root");
+echo "MySQL Server info: ".$mysql->host_info;
+
 ?>
